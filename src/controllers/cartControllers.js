3e0c7526f5149c9af5cee5244253b4459540b688
Leonardo-villagran <leonardovillagran@yahoo.com>
2023-07-20 18:42:01 -0400

    detalle_final

--- src/controllers/cartControllers.js
+++ src/controllers/cartControllers.js
@@ -6,8 +6,10 @@ const postCart = async (req, res) => {
         const datosBody = req.body;
         const { id_usuario } = req.datosToken;
         // console.log(datosBody);
-        await query.addToCart(id_usuario, datosBody);
-        return res.status(200).json({ mensaje: 'Orden de compra generada correctamente' });
+        const detalle_final=await query.addToCart(id_usuario, datosBody);
+        console.log(detalle_final);
+        return res.status(200).json({ mensaje: 'Orden de compra generada correctamente',
+        detalle_final: detalle_final  });
     } catch (error) {
         return res.status(500).json({ mensaje: error.message });
     }
