0a3f34064ea337ab3f6347a6d9e68c9d3803eeb0
Leonardo-villagran <leonardovillagran@yahoo.com>
2023-07-24 12:05:30 -0400

    Environment changed por production and development

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
-require('dotenv').config();
+const dotenv = require('dotenv');
 const express = require('express');
 const app = express();
 const cors = require('cors');
 const routes = require('./src/routes/routes');
 const databaseMiddleware = require('./src/middleware/databasereport');
 
-const PORT = process.env.PORT || 3000
+// Definir el entorno de ejecución (development, production, etc.)
+const environment = process.env.NODE_ENV || 'development';
 
-app.listen(PORT, () => {
-    console.log(`Server listening on ${PORT}`);
-})
+// Configurar dotenv para cargar las variables de entorno adecuadas
+if (environment === 'production') {
+    dotenv.config({ path: '.env.production' });
+} else {
+    dotenv.config({ path: '.env.development' });
+}
+
+const PORT = process.env.PORT || 3000
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -18,5 +24,11 @@ app.use(databaseMiddleware);
 app.use(cors());
 
 app.use('/', routes)
+
+
+app.listen(PORT, () => {
+    console.log(`Server listening on ${PORT}`);
+    console.log(`Tipo de ambiente: ${environment}`);
+})
 
 module.exports = app;
