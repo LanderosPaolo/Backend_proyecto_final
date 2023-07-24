9e23cf42112380ce911ec89c4300abf4caffc573
Leonardo-villagran <leonardovillagran@yahoo.com>
2023-07-24 13:08:07 -0400

    false

--- src/config/db.js
+++ src/config/db.js
@@ -1,11 +1,19 @@
 const { Pool } = require('pg');
 
+let ssl_value="";
+if (process.env.NODE_ENV==='development'){
+    ssl_value=true;
+}
+else{
+    ssl_value=false;
+}
+
 const pool = new Pool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    ssl:true, /* Solo con render externo */
+    ssl:ssl_value, /* Solo con render externo */
     allowExitOnIdle: true
 })
 
