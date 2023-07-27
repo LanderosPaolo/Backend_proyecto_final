98a2843b3c3c324b76dacae226e89bf14d92ffbb
Leonardo-villagran <leonardovillagran@yahoo.com>
2023-07-27 17:01:15 -0400

    Name added

--- src/models/cartModels.js
+++ src/models/cartModels.js
@@ -33,6 +33,7 @@ const changeEstado= async (id_usuario, datosBody) => {
 const addToCart = async (id_usuario, datosBody) => {
     /* informacion del usuario para obtener la direccion */
     const user = await userModel.getUserById(id_usuario);
+    const nombreUsario=user.nombre+" "+user.apellido;
     const direccion = user.direccion
     /* fin */
     const { detalle_productos } = datosBody;
@@ -84,7 +85,7 @@ const addToCart = async (id_usuario, datosBody) => {
     }
 
     let detalleP = detalle.map((objeto) => `id_producto: ${objeto.id_producto}, nombre: ${objeto.nombre.toUpperCase()}, número: ${numbers.formatLatin(objeto.numero)}, cantidad: ${numbers.formatLatin(objeto.cantidad)}, precio: ${numbers.formatLatin(objeto.precio)}, total: ${numbers.formatLatin(objeto.totalUnitario)}.`).join('<br/>');
-    detalleP += `<br/>Fecha de compra: ${fecha_venta}<br/>Dirección de entrega: ${direccion}`;
+    detalleP += `<br/>Fecha de compra: ${fecha_venta}<br/>Nombre usuario: ${nombreUsario}<br/>Dirección de entrega: ${direccion}`;
     const detalle_casi_final = "Registro de orden de compra: <br/>" + detalleP + '<br/>' + "El total a pagar corresponde a: " + numbers.formatLatin(total);
 
     //En caso de que no exista stock o que si exista 
@@ -92,17 +93,17 @@ const addToCart = async (id_usuario, datosBody) => {
     if (detalle_sin_stock.length != 0) {
         detalle_final = detalle_casi_final + "<br/> ----------------<br/>" + "Comics que no fueron agregados por falta de stock: <br/>" + detalle_sin_stock;
     } else {
-        detalle_final = detalle_casi_final
+        detalle_final = detalle_casi_final;
     }
     //Fin En caso de que no exista stock o que si exista 
     if (total === 0) {
         return 'No se genera orden de compra por que el total de venta es 0.'
     } else {
         console.log(detalle_final);
-        const queryOrdenDeCompra = 'INSERT INTO orden_compra (fecha_venta, detalle_productos, id_usuario) VALUES ($1, $2, $3)'
-        const params = [fechaActual, detalle_final, id_usuario]
+        const queryOrdenDeCompra = 'INSERT INTO orden_compra (fecha_venta, detalle_productos, id_usuario) VALUES ($1, $2, $3)';
+        const params = [fechaActual, detalle_final, id_usuario];
         // console.log(fechaActual);
-        await pool.query(queryOrdenDeCompra, params)
+        await pool.query(queryOrdenDeCompra, params);
         return detalle_final;
     }
 }
